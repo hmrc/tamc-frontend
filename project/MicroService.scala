595cbb98d92c574fc1d69f9622cd97e5de6fea0e
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2017-03-29 12:29:28 +0100

    DDCNLS-848: Changed Scala version 2.11.8.

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -58,6 +58,7 @@ trait MicroService {
     .settings(defaultSettings(): _*)
     .settings(
       targetJvm := "jvm-1.8",
+      scalaVersion := "2.11.8",
       libraryDependencies ++= appDependencies,
       parallelExecution in Test := false,
       fork in Test := false,
