6a6dede0ab1850e970bd27d9404c8e833bee75b4
Dane Watts <danewatts97@gmail.com>
2018-12-18 15:30:49 +0000

    DDCNOS-2265: Removed breadcrumb

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -21,9 +21,8 @@ import play.api.i18n.I18nSupport
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.partials.FormPartialRetriever
 import uk.gov.hmrc.renderer.TemplateRenderer
-import utils.TamcBreadcrumb
 
-trait BaseController extends FrontendController with I18nSupport with TamcBreadcrumb  {
+trait BaseController extends FrontendController with I18nSupport  {
   implicit val templateRenderer: TemplateRenderer = LocalTemplateRenderer
   implicit val formPartialRetriver: FormPartialRetriever = TamcFormPartialRetriever
 }
