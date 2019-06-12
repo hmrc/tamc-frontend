bad587c278e25f425b7f90826ef30b620dd81a16
Matthew McNally <matthew.mcnally@digital.hmrc.gov.uk>
2019-06-12 15:03:40 +0100

    DDCNLS-5971 - Upgraded to use latest libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,21 @@ import sbt._
 object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
    "uk.gov.hmrc" %% "emailaddress" % "3.2.0",
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "12.4.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "12.9.0",
     "com.ibm.icu" % "icu4j" % "54.1.1",
-    "uk.gov.hmrc" %% "http-caching-client" % "8.1.0",
+    "uk.gov.hmrc" %% "http-caching-client" % "8.4.0-play-25",
     "uk.gov.hmrc" %% "play-breadcrumb" % "1.0.0",
-    "uk.gov.hmrc" %% "local-template-renderer" % "2.3.0",
-    "uk.gov.hmrc" %% "time" % "3.2.0",
-    "uk.gov.hmrc" %% "domain" % "5.3.0",
-    "uk.gov.hmrc" %% "auth-client" % "2.19.0-play-25",
+    "uk.gov.hmrc" %% "local-template-renderer" % "2.4.0",
+    "uk.gov.hmrc" %% "time" % "3.6.0",
+    "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
+    "uk.gov.hmrc" %% "auth-client" % "2.22.0-play-25",
     "uk.gov.hmrc" %% "url-builder" % "3.1.0",
-    "uk.gov.hmrc" %% "play-partials" % "6.5.0",
+    "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-25",
     "uk.gov.hmrc" %% "tax-year" % "0.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % "3.5.0-play-25",
+        "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-25",
         "com.typesafe.play" %% "play-test" % PlayVersion.current,
         "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1",
         "org.jsoup" % "jsoup" % "1.11.3",
@@ -27,7 +27,7 @@ object AppDependencies {
         "org.mockito" % "mockito-core" % "2.24.5",
         "com.github.tomakehurst" % "wiremock-standalone" % "2.21.0"
       ).map(_ % "test")
-  
+
   val all: Seq[ModuleID] = compile ++ test
 }
 
