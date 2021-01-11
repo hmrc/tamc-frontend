49c7190f6f8f6c8deb798811f44590427a2a2cf7
john thirlaway <johnthirlaway95@gmail.com>
2021-01-11 10:05:50 +0000

    added changes from the play2.5 upgrade

---
+++ app/errorHandler/ErrorHandler.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package errorHandler
+
+import com.google.inject.Inject
+import config.TamcFormPartialRetriever
+import play.api.Configuration
+import play.api.i18n.{Lang, MessagesApi}
+import play.api.mvc.Request
+import play.twirl.api.Html
+import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
+import uk.gov.hmrc.renderer.TemplateRenderer
+
+class ErrorHandler @Inject()(
+                                val messagesApi: MessagesApi,
+                                val configuration: Configuration
+                            )
+                            (implicit
+                             templateRender: TemplateRenderer,
+                             lang: Lang
+                            ) extends FrontendErrorHandler {
+
+  implicit val formPartialRetriever = TamcFormPartialRetriever
+
+  private def lang(implicit request: Request[_]): Lang =
+    Lang(request.cookies.get("PLAY_LANG").fold("en")(_.value))
+
+
+  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]) = views.html.templates.error_template(pageTitle, heading, message)
+
+  override def notFoundTemplate(implicit request: Request[_]): Html = {
+    implicit val _: Lang = lang
+    views.html.templates.page_not_found_template(config.ApplicationConfig, formPartialRetriever)
+  }
+}
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,21 @@ import sbt._
 
 object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
-   "uk.gov.hmrc" %% "emailaddress" % "3.4.0",
+   "uk.gov.hmrc" %% "emailaddress" % "3.5.0",
     "uk.gov.hmrc" %% "bootstrap-play-25" % "5.6.0",
     "com.ibm.icu" % "icu4j" % "54.1.1",
-    "uk.gov.hmrc" %% "http-caching-client" % "8.4.0-play-25",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.2.0-play-25",
     "uk.gov.hmrc" %% "play-breadcrumb" % "1.0.0",
-    "uk.gov.hmrc" %% "local-template-renderer" % "2.4.0",
-    "uk.gov.hmrc" %% "time" % "3.6.0",
-    "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
-    "uk.gov.hmrc" %% "auth-client" % "2.22.0-play-25",
-    "uk.gov.hmrc" %% "url-builder" % "3.1.0",
+    "uk.gov.hmrc" %% "local-template-renderer" % "2.10.0-play-25",
+    "uk.gov.hmrc" %% "time" % "3.19.0",
+    "uk.gov.hmrc" %% "domain" % "5.10.0-play-25",
+    "uk.gov.hmrc" %% "auth-client" % "3.2.0-play-25",
+    "uk.gov.hmrc" %% "url-builder" % "3.4.0-play-25",
     "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-25",
-    "uk.gov.hmrc" %% "tax-year" % "0.5.0",
-    "uk.gov.hmrc" %% "play-language" % "3.4.0"
+    "uk.gov.hmrc" %% "tax-year" % "1.2.0",
+    "uk.gov.hmrc" %% "play-language" % "4.5.0-play-25",
+    "uk.gov.hmrc" %% "govuk-template" % "5.60.0-play-25",
+    "uk.gov.hmrc" %% "play-ui" % "8.19.0-play-25"
   )
 
   val test: Seq[ModuleID] = Seq(
