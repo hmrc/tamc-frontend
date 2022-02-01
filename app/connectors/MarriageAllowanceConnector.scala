a1c1448972658a0f380870b2f1378b06a16d52b0
Nj147 <nathanjackson147@gmail.com>
2022-02-01 12:47:06 +0000

    Removed assets-frontend and template provider from .conf

--- app/connectors/MarriageAllowanceConnector.scala
+++ app/connectors/MarriageAllowanceConnector.scala
@@ -24,7 +24,6 @@ import models._
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http._
 import uk.gov.hmrc.http.HttpClient
-import uk.gov.hmrc.http.HttpReads.Implicits._
 
 import scala.concurrent.{ExecutionContext, Future}
 
