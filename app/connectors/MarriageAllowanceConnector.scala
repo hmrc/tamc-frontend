cac8fe2ce4270b88ea69ef49d130690213e01d39
nik <ward.nickjames@gmail.com>
2022-04-12 15:51:21 +0100

    Update MarriageAllowanceConnector to use Either

--- app/connectors/MarriageAllowanceConnector.scala
+++ app/connectors/MarriageAllowanceConnector.scala
@@ -23,7 +23,8 @@ import errors.{BadFetchRequest, CitizenNotFound, TransferorNotFound}
 import models._
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http._
-import uk.gov.hmrc.http.HttpClient
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.HttpReadsInstances.readEitherOf
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -31,17 +32,44 @@ class MarriageAllowanceConnector @Inject()(httpClient: HttpClient, applicationCo
 
   def marriageAllowanceUrl = applicationConfig.marriageAllowanceUrl
 
-  def listRelationship(nino: Nino)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[RelationshipRecordList] =
-    httpClient.GET[RelationshipRecordStatusWrapper](s"$marriageAllowanceUrl/paye/$nino/list-relationship") map {
-      case RelationshipRecordStatusWrapper(relationshipRecordWrapper, ResponseStatus("OK")) => relationshipRecordWrapper
-      case RelationshipRecordStatusWrapper(_, ResponseStatus(TRANSFEROR_NOT_FOUND)) => throw TransferorNotFound()
-      case RelationshipRecordStatusWrapper(_, ResponseStatus(CITIZEN_NOT_FOUND)) => throw CitizenNotFound()
-      case RelationshipRecordStatusWrapper(_, ResponseStatus(BAD_REQUEST)) => throw BadFetchRequest()
-      case _ => throw new UnsupportedOperationException("Unable to handle list relationship request")
-    }
+  def listRelationship(nino: Nino)(
+    implicit hc: HeaderCarrier, ec: ExecutionContext): Future[RelationshipRecordList] =
+    httpClient
+      .GET[Either[UpstreamErrorResponse, HttpResponse]](
+        s"$marriageAllowanceUrl/paye/$nino/list-relationship"
+      )
+      .flatMap {
+        case Right(response) =>
+          val relationshipRecordWrapper = response.json.as[RelationshipRecordStatusWrapper]
+          relationshipRecordWrapper match {
+            case RelationshipRecordStatusWrapper(relationshipRecordWrapper, ResponseStatus("OK")) =>
+              Future.successful(relationshipRecordWrapper)
+            case RelationshipRecordStatusWrapper(_, ResponseStatus(TRANSFEROR_NOT_FOUND)) =>
+              Future.failed(TransferorNotFound())
+            case RelationshipRecordStatusWrapper(_, ResponseStatus(CITIZEN_NOT_FOUND)) =>
+              Future.failed(CitizenNotFound())
+            case RelationshipRecordStatusWrapper(_, ResponseStatus(BAD_REQUEST)) =>
+              Future.failed(BadFetchRequest())
+            case _ =>
+              Future.failed(
+                new UnsupportedOperationException("Unable to handle list relationship request")
+              )
+          }
+        case Left(error) =>
+          Future.failed(error)
+      }
 
-  def getRecipientRelationship(transferorNino: Nino, recipientData: RegistrationFormInput)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
-    httpClient.POST[RegistrationFormInput, HttpResponse](s"$marriageAllowanceUrl/paye/$transferorNino/get-recipient-relationship", body = recipientData)
+  def getRecipientRelationship(transferorNino: Nino, recipientData: RegistrationFormInput)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Either[UpstreamErrorResponse, GetRelationshipResponse]] =
+    httpClient
+      .POST[RegistrationFormInput, Either[UpstreamErrorResponse, HttpResponse]](
+        s"$marriageAllowanceUrl/paye/$transferorNino/get-recipient-relationship", body = recipientData
+      )
+      .map {
+        case Right(response) =>
+          Right(response.json.as[GetRelationshipResponse])
+        case Left(error) =>
+          Left(error)
+      }
 
   def createRelationship(transferorNino: Nino, data: CreateRelationshipRequestHolder
                         )(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
