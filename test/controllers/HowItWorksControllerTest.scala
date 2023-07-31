f7502eebcbbc86657e19d09b5d6c9ade8a349918
andrewbrown22 <45789050+andrewbrown22@users.noreply.github.com>
2023-07-31 11:20:42 +0100

    DDCNL-7626 Added Tests

---
+++ test/controllers/HowItWorksControllerTest.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import org.jsoup.Jsoup
+import play.api.http.Status.OK
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout}
+import utils.ControllerBaseTest
+
+class HowItWorksControllerTest extends ControllerBaseTest {
+
+  def howItWorksController: HowItWorksController = instanceOf[HowItWorksController]
+
+  "HowItWorksController" should {
+    "direct the user to How It Works page" in {
+      val result = howItWorksController.howItWorks()(request)
+
+      status(result) shouldBe OK
+      val document = Jsoup.parse(contentAsString(result))
+
+      document.title() shouldBe "Apply for Marriage Allowance - Marriage Allowance - GOV.UK"
+
+      val heading = document.getElementById("pageHeading").text
+      heading shouldBe "Apply for Marriage Allowance"
+
+      val button = document.getElementById("get-started")
+      button.text shouldBe "Start now to see if you are eligible for Marriage Allowance"
+    }
+  }
+}
+
