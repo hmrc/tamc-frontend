4da813d5b53cbb23b8f973510bf910ec2eae0512
Dandyboyo2 <45761631+Dandyboyo2@users.noreply.github.com>
2023-09-14 15:12:10 +0100

    DDCNL-7713 | WIP redirecting away from eligibility check

--- app/controllers/EligibilityController.scala
+++ app/controllers/EligibilityController.scala
@@ -19,15 +19,9 @@ package controllers
 import config.ApplicationConfig
 import controllers.actions.UnauthenticatedActionTransformer
 import forms.EligibilityCalculatorForm.calculatorForm
-import forms.MultiYearDateOfBirthForm.dateOfBirthForm
-import forms.MultiYearDoYouLiveInScotlandForm.doYouLiveInScotlandForm
-import forms.MultiYearDoYouWantToApplyForm.doYouWantToApplyForm
-import forms.MultiYearEligibilityCheckForm.eligibilityForm
-import forms._
 import models.Country
-import play.api.mvc.{Action, AnyContent, Call, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.EligibilityCalculatorService
-import utils.isScottishResident
 
 import javax.inject.Inject
 
@@ -36,129 +30,120 @@ class EligibilityController @Inject()(
   eligibilityCalculatorService: EligibilityCalculatorService,
   appConfig: ApplicationConfig,
   cc: MessagesControllerComponents,
-  eligibilityCheckView: views.html.multiyear.eligibility_check,
-  eligibilityNonEligibleFinishView: views.html.multiyear.eligibility_non_eligible_finish,
-  dateOfBirthCheckView: views.html.multiyear.date_of_birth_check,
-  doYouLiveInScotlandView: views.html.multiyear.do_you_live_in_scotland,
-  lowerEarnerView: views.html.multiyear.lower_earner,
-  partnersIncomeQuestionView: views.html.multiyear.partners_income_question,
-  doYouWantToApplyView: views.html.multiyear.do_you_want_to_apply,
   calculatorView: views.html.calculator,
   ptaCalculatorView: views.html.pta.calculator,
-  multiYearPartnersIncomeQuestionForm: MultiYearPartnersIncomeQuestionForm,
-  multiYearLowerEarnerForm: MultiYearLowerEarnerForm) extends BaseController(cc) {
+  howItWorksView: views.html.multiyear.how_it_works) extends BaseController(cc) {
 
   def eligibilityCheck(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
-      Ok(eligibilityCheckView(eligibilityCheckForm = eligibilityForm))
+      Ok(howItWorksView())
   }
 
-  def eligibilityCheckAction(): Action[AnyContent] = {
-
-    def finishUrl(isLoggedIn: Boolean): String =
-      if (isLoggedIn) appConfig.ptaFinishedUrl else appConfig.gdsFinishedUrl
-
-    unauthenticatedAction {
-      implicit request =>
-        eligibilityForm.bindFromRequest().fold(
-          formWithErrors =>
-            BadRequest(eligibilityCheckView(formWithErrors)),
-          eligibilityInput => {
-            if (eligibilityInput.married) {
-              Redirect(controllers.routes.EligibilityController.dateOfBirthCheck)
-            } else {
-              Ok(eligibilityNonEligibleFinishView(finishUrl(request.isAuthenticated)))
-            }
-          })
-    }
-  }
+//  def eligibilityCheckAction(): Action[AnyContent] = {
+//
+//    def finishUrl(isLoggedIn: Boolean): String =
+//      if (isLoggedIn) appConfig.ptaFinishedUrl else appConfig.gdsFinishedUrl
+//
+//    unauthenticatedAction {
+//      implicit request =>
+//        eligibilityForm.bindFromRequest().fold(
+//          formWithErrors =>
+//            BadRequest(eligibilityCheckView(formWithErrors)),
+//          eligibilityInput => {
+//            if (eligibilityInput.married) {
+//              Redirect(controllers.routes.EligibilityController.dateOfBirthCheck)
+//            } else {
+//              Ok(eligibilityNonEligibleFinishView(finishUrl(request.isAuthenticated)))
+//            }
+//          })
+//    }
+//  }
 
   def dateOfBirthCheck(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
-      Ok(dateOfBirthCheckView(dateOfBirthForm))
+      Ok(howItWorksView())
   }
 
-  def dateOfBirthCheckAction(): Action[AnyContent] = unauthenticatedAction {
-    implicit request =>
-      dateOfBirthForm.bindFromRequest().fold(
-        formWithErrors =>
-          BadRequest(dateOfBirthCheckView(formWithErrors)),
-        _ => {
-          Redirect(controllers.routes.EligibilityController.doYouLiveInScotland)
-        })
-  }
+//  def dateOfBirthCheckAction(): Action[AnyContent] = unauthenticatedAction {
+//    implicit request =>
+//      dateOfBirthForm.bindFromRequest().fold(
+//        formWithErrors =>
+//          BadRequest(dateOfBirthCheckView(formWithErrors)),
+//        _ => {
+//          Redirect(controllers.routes.EligibilityController.doYouLiveInScotland)
+//        })
+//  }
 
   def doYouLiveInScotland(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
-      Ok(doYouLiveInScotlandView(doYouLiveInScotlandForm = doYouLiveInScotlandForm))
-        .withSession(request.session - appConfig.SCOTTISH_RESIDENT)
+      Ok(howItWorksView())
   }
 
-  def doYouLiveInScotlandAction(): Action[AnyContent] = unauthenticatedAction {
-    implicit request =>
-      doYouLiveInScotlandForm.bindFromRequest().fold(
-        formWithErrors =>
-          BadRequest(doYouLiveInScotlandView(formWithErrors)),
-        doYouLiveInScotlandInput => {
-          Redirect(controllers.routes.EligibilityController.lowerEarnerCheck)
-            .withSession(request.session + (appConfig.SCOTTISH_RESIDENT -> doYouLiveInScotlandInput.doYouLiveInScotland.toString))
-        })
-  }
+//  def doYouLiveInScotlandAction(): Action[AnyContent] = unauthenticatedAction {
+//    implicit request =>
+//      doYouLiveInScotlandForm.bindFromRequest().fold(
+//        formWithErrors =>
+//          BadRequest(doYouLiveInScotlandView(formWithErrors)),
+//        doYouLiveInScotlandInput => {
+//          Redirect(controllers.routes.EligibilityController.lowerEarnerCheck)
+//            .withSession(request.session + (appConfig.SCOTTISH_RESIDENT -> doYouLiveInScotlandInput.doYouLiveInScotland.toString))
+//        })
+//  }
 
   def lowerEarnerCheck(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
-      Ok(lowerEarnerView(multiYearLowerEarnerForm.lowerEarnerForm))
+      Ok(howItWorksView())
   }
 
-  def lowerEarnerCheckAction(): Action[AnyContent] = unauthenticatedAction {
-    implicit request =>
-      multiYearLowerEarnerForm.lowerEarnerForm.bindFromRequest().fold(
-        formWithErrors =>
-          BadRequest(lowerEarnerView(formWithErrors)),
-        _ => {
-          Redirect(controllers.routes.EligibilityController.partnersIncomeCheck)
-        })
-  }
+//  def lowerEarnerCheckAction(): Action[AnyContent] = unauthenticatedAction {
+//    implicit request =>
+//      multiYearLowerEarnerForm.lowerEarnerForm.bindFromRequest().fold(
+//        formWithErrors =>
+//          BadRequest(lowerEarnerView(formWithErrors)),
+//        _ => {
+//          Redirect(controllers.routes.EligibilityController.partnersIncomeCheck)
+//        })
+//  }
 
   def partnersIncomeCheck(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
-      Ok(partnersIncomeQuestionView(multiYearPartnersIncomeQuestionForm.partnersIncomeForm, isScottishResident(request)))
+      Ok(howItWorksView())
   }
 
-  def partnersIncomeCheckAction(): Action[AnyContent] = unauthenticatedAction {
-    implicit request =>
-      multiYearPartnersIncomeQuestionForm.partnersIncomeForm.bindFromRequest().fold(
-        formWithErrors =>
-          BadRequest(partnersIncomeQuestionView(formWithErrors, isScottishResident(request))),
-        _ => {
-          Redirect(controllers.routes.EligibilityController.doYouWantToApply)
-        })
-  }
+//  def partnersIncomeCheckAction(): Action[AnyContent] = unauthenticatedAction {
+//    implicit request =>
+//      multiYearPartnersIncomeQuestionForm.partnersIncomeForm.bindFromRequest().fold(
+//        formWithErrors =>
+//          BadRequest(partnersIncomeQuestionView(formWithErrors, isScottishResident(request))),
+//        _ => {
+//          Redirect(controllers.routes.EligibilityController.doYouWantToApply)
+//        })
+//  }
 
   def doYouWantToApply(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
-      Ok(doYouWantToApplyView(doYouWantToApplyForm = doYouWantToApplyForm))
+      Ok(howItWorksView())
   }
 
-  def doYouWantToApplyAction(): Action[AnyContent] = {
-
-    def finishUrl(isLoggedIn: Boolean): String =
-      if (isLoggedIn) appConfig.ptaFinishedUrl else appConfig.gdsFinishedUrl
-
-    unauthenticatedAction {
-      implicit request =>
-        doYouWantToApplyForm.bindFromRequest().fold(
-          formWithErrors =>
-            BadRequest(doYouWantToApplyView(formWithErrors)),
-          doYouWantToApplyInput => {
-            if (doYouWantToApplyInput.doYouWantToApply) {
-              Redirect(controllers.routes.TransferController.transfer)
-            } else {
-              Redirect(Call("GET", finishUrl(request.isAuthenticated)))
-            }
-          })
-    }
-  }
+//  def doYouWantToApplyAction(): Action[AnyContent] = {
+//
+//    def finishUrl(isLoggedIn: Boolean): String =
+//      if (isLoggedIn) appConfig.ptaFinishedUrl else appConfig.gdsFinishedUrl
+//
+//    unauthenticatedAction {
+//      implicit request =>
+//        doYouWantToApplyForm.bindFromRequest().fold(
+//          formWithErrors =>
+//            BadRequest(doYouWantToApplyView(formWithErrors)),
+//          doYouWantToApplyInput => {
+//            if (doYouWantToApplyInput.doYouWantToApply) {
+//              Redirect(controllers.routes.TransferController.transfer)
+//            } else {
+//              Redirect(Call("GET", finishUrl(request.isAuthenticated)))
+//            }
+//          })
+//    }
+//  }
 
   def gdsCalculator(): Action[AnyContent] = unauthenticatedAction {
     implicit request =>
